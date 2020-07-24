64407c1ff5b06d803c68bf2a19bdd367aeb50e9b
Kennedythacode <johnkennedyx1212@gmail.com>
2020-07-24 07:56:09 -0700

    fixed common_passwords spelling

--- validator/validator.py
+++ validator/validator.py
@@ -5,28 +5,37 @@ class Validator:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
+        
     def validate_username_not_password(self):
         if not search(self.password, self.username):
             return True
         else:
             print("password and username are not similar")
             return False
+
     def password_not_common(self):
         with open("common_passwords.txt", "r") as c:
-            if not self.password in c.read():
+            if self.password not in c.read():
                 return True
             else:
                 return False
     
-    def minimum_password_length(self):
+    def minimum_length(self, min_length):
+        if self.password >= min_length:
+            return True
+        else:
+            return False
+
+    def contains_alphanumeric_and_symbols(self):
+        contains_num_and_letter = re.search("[A-Za-z0-9]+", password)
+        contains_special_character = re.search("[!@#$%^&*]+", password)
+        if contains_num_and_letter and contains_special_character:
+            return True
+        else:
+            return False
         
-
-
-        
-
-if __name__ == "__main__":
-    run()
-
-
-
+    def confirm_password(self, password2):
+        if self.password == password2:
+            return True
+        else:
+            return False
